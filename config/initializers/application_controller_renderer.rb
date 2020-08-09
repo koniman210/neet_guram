8e33b07f0c443140754917a2fdff4913aa24d7a2
koniman210 <koniman1123@gmail.com>
2020-08-09 10:13:56 +0900

    ニートグラムアプリ

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'c1a4f6d5676b0ffbab9ccf04097ee1e473417d9b14d941bdbbee9aa7f21807e61302038de96aef4985a5403ba936fe102d6154d4ac0dff9edd16ba49a2e09c2b'
